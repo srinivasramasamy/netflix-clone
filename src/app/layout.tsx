94427d7cd89f34472d0dbb4ca2dde02221069adf
Srinivas Ramasamy <srinivasramasami@gmail.com>
2023-12-20 13:02:04 -0500

    Add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Netflix Clone",
   description: "This is a cloned verson of netflix for learning.",
+  icons: "https://assets.nflxext.com/us/ffe/siteui/common/icons/nficon2023.ico",
 };
 
 export default function RootLayout({
