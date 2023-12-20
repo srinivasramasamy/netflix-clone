427072df46e80cdaaabcc665cc4a54a27327b4a5
Srinivas Ramasamy <srinivasramasami@gmail.com>
2023-12-20 13:24:48 -0500

    Add background image for login screen

--- src/app/components/login/Login.tsx
+++ src/app/components/login/Login.tsx
@@ -4,6 +4,7 @@ function Login() {
   return (
     <div>
       <Header />
+      <img src="https://assets.nflxext.com/ffe/siteui/vlv3/563192ea-ac0e-4906-a865-ba9899ffafad/8b07ca45-0d02-4689-ad4d-79cf71fb0314/US-en-20231218-popsignuptwoweeks-perspective_alpha_website_small.jpg" />
     </div>
   );
 }
