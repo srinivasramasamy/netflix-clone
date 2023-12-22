b7efd9dd3a1c427e97dab1034f92e918a3bdfc88
Srinivas Ramasamy <srinivasramasami@gmail.com>
2023-12-22 09:17:50 -0500

    Update brightness

--- src/app/components/login/Login.tsx
+++ src/app/components/login/Login.tsx
@@ -5,10 +5,13 @@ function Login() {
     <div>
       <Header />
       <div className="absolute">
-        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/563192ea-ac0e-4906-a865-ba9899ffafad/8b07ca45-0d02-4689-ad4d-79cf71fb0314/US-en-20231218-popsignuptwoweeks-perspective_alpha_website_small.jpg" />
+        <img
+          className="brightness-50"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/563192ea-ac0e-4906-a865-ba9899ffafad/8b07ca45-0d02-4689-ad4d-79cf71fb0314/US-en-20231218-popsignuptwoweeks-perspective_alpha_website_small.jpg"
+        />
       </div>
       <div>
-        <form className="absolute flex flex-col gap-4 w-1/3 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-[.85] p-4 text-white">
+        <form className="absolute flex flex-col gap-4 w-1/3 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-[.70] p-4 text-white">
           <h1 className="mx-10 h-10 text-3xl">Sign In</h1>
           <input
             className="mx-10 h-12 bg-[#333] px-5 rounded"
