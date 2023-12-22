27ae6d9e5cecab2adc2c00a0227b36754ea75d34
Srinivas Ramasamy <srinivasramasami@gmail.com>
2023-12-22 08:34:15 -0500

    Update login form

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,6 @@
 function Header() {
   return (
-    <div className="absolute bg-gradient-to-b from-black w-full">
+    <div className="absolute bg-gradient-to-b from-black w-full z-10">
       <img
         className="w-52 ml-4"
         src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
--- src/app/components/login/Login.tsx
+++ src/app/components/login/Login.tsx
@@ -4,7 +4,27 @@ function Login() {
   return (
     <div>
       <Header />
-      <img src="https://assets.nflxext.com/ffe/siteui/vlv3/563192ea-ac0e-4906-a865-ba9899ffafad/8b07ca45-0d02-4689-ad4d-79cf71fb0314/US-en-20231218-popsignuptwoweeks-perspective_alpha_website_small.jpg" />
+      <div className="absolute">
+        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/563192ea-ac0e-4906-a865-ba9899ffafad/8b07ca45-0d02-4689-ad4d-79cf71fb0314/US-en-20231218-popsignuptwoweeks-perspective_alpha_website_small.jpg" />
+      </div>
+      <div>
+        <form className="absolute flex flex-col gap-4 w-1/3 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-[.85] p-4 text-white">
+          <h1 className="mx-10 h-10 text-3xl">Sign In</h1>
+          <input
+            className="mx-10 h-12 bg-[#333] px-5 rounded"
+            type="text"
+            placeholder="Email or phone number"
+          />
+          <input
+            className="mx-10 h-12 bg-[#333] px-5 rounded"
+            type="password"
+            placeholder="Password"
+          />
+          <button className="bg-red-700 mx-10 h-12 my-6 rounded">
+            Sign In
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
