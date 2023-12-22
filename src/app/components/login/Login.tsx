9d6b0c84d31e096d08c3bb95b7bba49fde758b84
Srinivas Ramasamy <srinivasramasami@gmail.com>
2023-12-22 14:15:23 -0500

    Update login page

--- src/app/components/login/Login.tsx
+++ src/app/components/login/Login.tsx
@@ -12,7 +12,7 @@ function Login() {
       </div>
       <div>
         <form className="absolute flex flex-col gap-4 w-1/3 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-[.70] p-4 text-white">
-          <h1 className="mx-10 h-10 text-3xl">Sign In</h1>
+          <h1 className="mx-10 mt-10 h-10 text-3xl">Sign In</h1>
           <input
             className="mx-10 h-12 bg-[#333] px-5 rounded"
             type="text"
@@ -40,6 +40,15 @@ function Login() {
             <a className="text-[#4c4c4c]" href="#">
               Need help?
             </a>
+          </div>
+          <div className=" my-20 mx-10 text-[#656565]">
+            <div className="mt-2">
+              New to Netflix? <span className="text-white">Sign up now</span>.
+            </div>
+            <div className="text-xs mt-2">
+              This page is protected by Google reCAPTCHA to ensure you're not a
+              bot. <span className="text-blue-600">Learn more.</span>
+            </div>
           </div>
         </form>
       </div>
