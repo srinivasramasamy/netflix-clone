28d2be2d5abcbaaeef39d0f18501fcb4488ea77d
Srinivas Ramasamy <srinivasramasami@gmail.com>
2023-12-27 11:38:40 -0500

    Update footer

--- src/app/components/login/Footer.tsx
+++ src/app/components/login/Footer.tsx
@@ -1,19 +1,23 @@
 function Footer() {
   return (
-    <div className="absolute w-full  bg-black bg-opacity-[.75] h-1/3 -bottom-1/2 text-[#656565] text-xs">
-      <div className="flex flex-wrap mx-32 gap-y-3">
-        <a className="basis-1/4">FAQ</a>
-        <a className="basis-1/4">Help Center</a>
-        <a className="basis-1/4">Netflix Shop</a>
-        <a className="basis-1/4">Terms of Use</a>
-        <a className="basis-1/4">Privacy</a>
-        <a className="basis-1/4">Cookie Preferences</a>
-        <a className="basis-1/4">Corporate Information</a>
-        <a className="basis-1/4">
-          Do Not Sell or Share My Personal <br />
-          Information
-        </a>
-        <a className="basis-1/4">Ad Choices</a>
+    <div className="absolute w-full  bg-black bg-opacity-[.75] h-[45%] -bottom-1/2 text-[#656565] text-xs">
+      <div className="flex flex-col mx-32 my-5 gap-y-8">
+        <div className="text-sm">Questions? Call 1-844-505-2993</div>
+        <div className="flex flex-wrap  gap-y-4">
+          <a className="basis-1/4">FAQ</a>
+          <a className="basis-1/4">Help Center</a>
+          <a className="basis-1/4">Netflix Shop</a>
+          <a className="basis-1/4">Terms of Use</a>
+          <a className="basis-1/4">Privacy</a>
+          <a className="basis-1/4">Cookie Preferences</a>
+          <a className="basis-1/4">Corporate Information</a>
+          <a className="basis-1/4">
+            Do Not Sell or Share My Personal <br />
+            Information
+          </a>
+          <a className="basis-1/4">Ad Choices</a>
+        </div>
+        <div>English</div>
       </div>
     </div>
   );
